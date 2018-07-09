2bb17d0fbb4b6cda37495ff8ee0672b866388da5
idlesolution <kappakeepo@o2.pl>
2018-07-09 15:18:42 +0200

    asdasfsa

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -159,6 +159,17 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'solutionidle@gmail.com'
 EMAIL_HOST_PASSWORD = 'cbdum123'
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+)
+
 
 
 
